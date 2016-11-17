f798dad4538ee46cdfd81386f2f1fd2a937ee475
Tsuyoshi Tanaka <tanakatsu1080@gmail.com>
2016-11-17 10:47:46 +0900

    Enable to specify awc-cli profile name

--- python/deploy.sh
+++ python/deploy.sh
@@ -5,8 +5,21 @@ echo 'Deployment started'
 # lambda-uploader
 # https://github.com/rackerlabs/lambda-uploader
 
+if [ -n "$1" ]; then
+  profile=$2
+else
+  profile='default'
+fi
+echo profile=${profile}
+
+if [ -e lambda_function.zip ]; then
+  rm lambda_function.zip
+  echo 'removed lambda_function.zip'
+fi
+
 cp -r PIL_ec2 PIL
-lambda-uploader # or lambda-uploader --profile=altenative-profile
+lambda-uploader -V --profile=${profile}
+
 rm -r PIL
 
-echo 'finished successfully'
+echo 'finished'
