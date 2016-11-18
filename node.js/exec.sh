f08893223b806d824730f07a01ceef81febc687f
Tsuyoshi Tanaka <tanakatsu1080@gmail.com>
2016-11-18 11:34:59 +0900

    Update exec.sh

--- node.js/exec.sh
+++ node.js/exec.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+
+#
+# AWS-CLI is required. Install aws-cli on your machine.
+#
+
+# Set environment variables
+export $(cat .env | xargs)
+echo Updated environment variables
+
+# Invoke lambda function
+echo Invoking...
 aws lambda invoke --invocation-type RequestResponse --function-name NodeThumb --payload file://event.json outputfile.txt
